78701ba65c17dc58b32f5e87e90c43cc86130b95
navgarg <navgarg06@gmail.com>
2025-05-28 16:49:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:crux_notes/services/auth_gate.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope( // Wrap with ProviderScope
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Notes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light, // Default to light
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        // Define dark theme colors if needed
+      ),
+      themeMode: ThemeMode.system, // Or allow user to choose
+      home: const AuthGate(), // Entry point will check auth state
+    );
+  }
+}
